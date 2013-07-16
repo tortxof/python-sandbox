4de9d966576c79f4f5d89bf518ff2c5465d31ebf
Daniel Jones <tortxof@gmail.com>
2013-07-16 00:32:40 -0400

    added player name input routine.

--- farkle-score.py
+++ farkle-score.py
@@ -1,1 +1,16 @@
 #! /usr/bin/env python3.2
+
+print("Enter player names. Enter blank line for no new players.")
+players = []
+
+while True:
+    newplayer = input('Player name: ')
+    if newplayer != '':
+        players.append(newplayer)
+    else:
+        break
+
+print("\nPlayers:")
+for i in players:
+    print(i)
+
