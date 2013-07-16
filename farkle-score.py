a92e731b0269d3381e4f2c51678f8eebf5b6d7ae
Daniel Jones <tortxof@gmail.com>
2013-07-16 01:49:47 -0400

    Finished first working version of farkle-score.

--- farkle-score.py
+++ farkle-score.py
@@ -1,7 +1,28 @@
 #! /usr/bin/env python3.2
 
-print("Enter player names. Enter blank line for no new players.")
+import sys
+
+gameOver = False
+round = 0
 players = []
+scores = []
+scoreLimit = 10000
+
+def printScores():
+    global gameOver
+    print("Current scores:")
+    for i in range(len(players)):
+        print(players[i], "\n\t", scores[i],)
+        if scores[i] >= scoreLimit:
+            print(players[i], " is the winner!")
+            gameOver = True
+    print("\n")
+    if gameOver:
+        print("Game Over")
+        sys.exit(0)
+
+
+print("Enter player names. Enter blank line for no new players.")
 
 while True:
     newplayer = input('Player name: ')
@@ -9,8 +30,22 @@ while True:
         players.append(newplayer)
     else:
         break
+print("\n")
 
 print("\nPlayers:")
 for i in players:
     print(i)
+print("\n")
 
+for i in range(len(players)):
+    scores.append(0)
+
+while True:
+    round += 1
+    print("Round ", round)
+    for i in range(len(players)):
+        print(players[i], "'s turn.")
+        turnScore = int(input("Score for this turn: "))
+        scores[i] += turnScore
+        print("\n")
+        printScores()
