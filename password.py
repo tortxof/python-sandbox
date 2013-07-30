af54a209d2f51750e69c5b7463d5a911d27fbe17
Daniel Jones <tortxof@gmail.com>
2013-07-30 00:53:55 -0400

    adding newPassword function to passwords.py

--- password.py
+++ password.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3
 
 import sqlite3
+import subprocess
 
-pwdatabase = '/home/tortxof/private/passwords.db'
+# pwdatabase = '/home/tortxof/private/passwords.db'
+pwdatabase = ':memory:'
 
 def pwSearch(query):
     conn = sqlite3.connect(pwdatabase)
@@ -15,7 +17,23 @@ def showResult(result):
             print(field)
         print()
 
+def newPassword():
+    conn = sqlite3.connect(pwdatabase)
+    print('Creating new password entry.')
+    newrecord = ['' for i in range(5)]
+    newrecord[0] = input('Title: ')
+    newrecord[1] = input('URL: ')
+    newrecord[2] = input('Username: ')
+    newrecord[3] = subprocess.check_output(['pwgen','-cn','12','1']).decode().strip()
+    newrecord[4] = input('Other: ')
+    conn.execute("create table passwords ('title', 'url', 'username', 'password', 'other')")
+    conn.execute('insert into passwords values (?, ?, ?, ?, ?)', newrecord)
+    conn.commit()
+    for i in conn.execute('select * from passwords'):
+        print(i)
+
 def getCmd():
     pass
 
-pwSearch(input("Search: "))
+# pwSearch(input("Search: "))
+newPassword()
