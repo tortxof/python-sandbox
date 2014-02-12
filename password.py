4d68b9c2df6cbffb6029ee6afda2fb446372cb15
Daniel Jones <tortxof@gmail.com>
2014-02-11 22:45:36 -0500

    removed unused code.

--- password.py
+++ password.py
@@ -70,10 +70,7 @@ html_results = """
 </table>
 </div>
 """
-
-prompt = '> '
 headers = ('Title','URL','Username','Password','Other')
-
 
 def pwSearch(query):
     conn = sqlite3.connect(pwdatabase)
@@ -86,28 +83,6 @@ def showResult(result):
     for row in result:
         out += html_results.format(headers=headers,title=row[0],url=row[1],username=row[2],password=row[3],other=row[4])
     return out
-
-def newPassword():
-    print('Creating new password entry.')
-    newrecord = ['' for i in range(5)]
-    newrecord[0] = input(headers[0] + ': ')
-    newrecord[1] = input(headers[1] + ': ')
-    newrecord[2] = input(headers[2] + ': ')
-    newrecord[3] = subprocess.check_output(['pwgen','-cn','12','1']).decode().strip()
-    newrecord[4] = input(headers[4] + ': ')
-    print('Add this record to database?\n')
-    for i in range(len(headers)):
-        print(headers[i] + ': ' + newrecord[i])
-    print()
-    if input(prompt) == 'y':
-        conn = sqlite3.connect(pwdatabase)
-        conn.execute('insert into passwords values (?, ?, ?, ?, ?)', newrecord)
-        conn.commit()
-        conn.close()
-    else:
-        print('Aborted')
-
-
 
 class Root(object):
     def index(self):
@@ -134,17 +109,3 @@ class Root(object):
     add.exposed = True
 
 cherrypy.quickstart(Root())
-
-
-'''
-while True:
-    print('(a)dd new password\n(s)earch for password\n(q)uit')
-    cmd = input(prompt)
-
-    if cmd == 'a':
-        newPassword()
-    elif cmd == 's':
-        pwSearch(input('Search: '))
-    else:
-        break
-'''
