1158693d0c91f69b505cea4e5b03aa9e31e73ea5
Daniel Jones <tortxof@gmail.com>
2014-07-08 21:45:01 -0400

    Rewrote program as function.

--- basex.py
+++ basex.py
@@ -5,29 +5,19 @@ base58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 alphabet = base58
 
 
-def numduplicates(alphabet):
-    count = 0
+def hasDuplicates(alphabet):
     for i in alphabet:
         if alphabet.count(i) > 1:
-            count += 1
-    return(count)
+            return True
+    return False
 
-if numduplicates(alphabet) > 0:
-    print('Duplicate characters in alphabet.')
-    exit(1)
 
-number = int(input('Enter a positive integer:'))
-
-hash = ''
-
-if number == 0:
-    hash = alphabet[0]
-
-while number:
-    remainder = number % len(alphabet)
-    number = number // len(alphabet)
-    hash += alphabet[remainder]
-
-hash = hash[::-1]
-
-print(hash)
+def basex(number, alphabet):
+    myHash = ''
+    if number == 0:
+        myHash = alphabet[0]
+    while number > 0:
+        remainder = number % len(alphabet)
+        number = number // len(alphabet)
+        myHast += alphabet[remainder]
+    return myHash[::-1]
