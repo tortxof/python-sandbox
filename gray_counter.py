f4509dc5070ff5fcf2e7590619c795c866295a71
Daniel Jones <tortxof@gmail.com>
2014-07-18 20:23:00 -0400

    Made gray_counter usable as module.

--- gray_counter.py
+++ gray_counter.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 
-for i in range(256):
-    gray = (i >> 1) ^ i
-    print('{0:08b}'.format(gray))
+def binToGray(i):
+    return (i >> 1) ^ i
+
+if __name__ == '__main__':
+    for i in range(256):
+        print('{0:08b}'.format(binToGray(i)))
