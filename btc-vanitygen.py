68be8e396b6f1494dab51ac3c0eb2029ae5ec4d9
Daniel Jones <tortxof@gmail.com>
2014-08-05 13:33:01 -0400

    Added bitcoin vanity address generator using pycoin.

---
+++ btc-vanitygen.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+import os
+from pycoin.key import Key
+
+def vanitygen(s):
+    s = s.lower()
+    while True:
+        r = int.from_bytes(os.urandom(32), 'big')
+        k = Key(random_exponent=r)
+        if k.address()[:len(s)+1].lower() == '1' + s:
+            print('Address:', k.address())
+            print('WIF:'. k.wif())
