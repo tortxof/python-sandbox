93ec184101e6ab1112d237ff880f41a1cc54d33a
Daniel Jones <tortxof@gmail.com>
2014-08-05 13:37:28 -0400

    Used the wrong word.

--- btc-vanitygen.py
+++ btc-vanitygen.py
@@ -7,7 +7,7 @@ def vanitygen(s):
     s = s.lower()
     while True:
         r = int.from_bytes(os.urandom(32), 'big')
-        k = Key(random_exponent=r)
+        k = Key(secret_exponent=r)
         if k.address()[:len(s)+1].lower() == '1' + s:
             print('Address:', k.address())
             print('WIF:'. k.wif())
