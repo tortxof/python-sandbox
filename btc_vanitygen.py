1a2ab849b3e6f3ae5ba95f0129ed0218fc23eeba
Daniel Jones <tortxof@gmail.com>
2014-08-05 14:44:40 -0400

    Moved some things out of the loop.

--- btc_vanitygen.py
+++ btc_vanitygen.py
@@ -4,10 +4,11 @@ import os
 from pycoin.key import Key
 
 def vanitygen(s):
-    s = s.lower()
+    s = '1' + s.lower()
+    l = len(s)
     while True:
         r = int.from_bytes(os.urandom(32), 'big')
         k = Key(secret_exponent=r)
-        if k.address()[:len(s)+1].lower() == '1' + s:
+        if k.address()[:l] == s:
             print('Address:', k.address())
             print('WIF:'. k.wif())
